44ef7a42fe313ddbeb8e638d6a1f166c7a5d3f4f
Brian Rhodes <brianr4548@gmail.com>
2020-11-03 21:10:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(yaml, english_emoticon)
   lib = load_library(yaml)
     lib.each do |key, value|
-      english_emoticon == lib[:japanese]
+      english_emoticon == value[:japanese]
       return key
     end
    
