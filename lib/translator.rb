7811584ffce5403fe74febdef36aaef75246fda0
Brian Rhodes <brianr4548@gmail.com>
2020-11-03 20:54:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_english_meaning(yaml, japanese_emoticon)
     if japanese_emoticon == value[:japanese]
       return key
     else
-      "Sorry, that emoticon was not found"
+      return "Sorry, that emoticon was not found"
     end
   end 
 end
